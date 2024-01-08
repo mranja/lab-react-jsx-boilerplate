d4e6fd630708066215a02bda87360217b3e8c5c7
RANJAN <141898565+mranja@users.noreply.github.com>
2024-01-08 14:47:40 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,25 @@ const imageData = ()=>{
 }
 
 function App() {
-  // code here
+  
+  const image = imageData()
+
+  return (
+
+    <div>
+      <h3>Kalvium Gallery</h3>
+      <div id='parent'>
+        {image.map(element => {
+          return <div>
+            <img src={element.img} alt="image" />
+          </div>
+        })}
+      </div>
+      
+    </div>
+   
+  )
+
 }
 
 export default App;
